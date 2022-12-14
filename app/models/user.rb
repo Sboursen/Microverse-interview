8cb00d2a57f709b03dac0cfc28b2c19a641b4033
Soufiane Boursen <dev.sboursen@gmail.com>
2022-12-14 11:33:17 +0100

    Create User model and run migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true, length: { maximum: 250 }
+  validates :last_name, presence: true, length: { maximum: 250 }
+  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 250 }, format: { with: EMAIL_FORMAT }
+  validates :status, presence: true, inclusion: { in: %w[Active Inactive],
+                                                  message: '%<value>s is not a valid status' }
 end
